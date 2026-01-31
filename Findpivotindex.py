f1fa32e21bfd38dd581f1108591b7baad4e1c81e
ALUBILLI JYANESWAR RAO <alubillijyaneswarrao@gmail.com>
2026-01-31 21:41:39 +0530

    Refactor comments and structure in Findpivotindex.py
    Updated comments and formatting in Findpivotindex.py.

---
+++ Findpivotindex.py
@@ -0,0 +1,28 @@
+# 724. Find Pivot Index
+# Given an array of integers nums, calculate the pivot index of this array.
+
+# The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum of all the numbers strictly to the index's right.
+
+# If the index is on the left edge of the array, then the left sum is 0 because there are no elements to the left. This also applies to the right edge of the array.
+
+# Return the leftmost pivot index. If no such index exists, return -1.
+
+class Solution(object):
+    def pivotIndex(self, nums):
+        n = len(nums)
+        
+        for l in range(n):
+            sumi = 0
+            sumj = 0
+            
+            for i in range(l):
+                sumi += nums[i]
+            
+            for j in range(l+1, n):
+                sumj += nums[j]
+            
+            if sumi == sumj:
+                print(l)
+                return l   
+        
+        return -1  
