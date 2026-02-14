5d85b3fc7ade8f6547de1efa3b86cd99d855b7fc
ALUBILLI JYANESWAR RAO <alubillijyaneswarrao@gmail.com>
2026-02-14 06:11:53 +0530

    Add solution for Contains Duplicate II problem
    Implemented a solution to check for nearby duplicates in an array.

---
+++ 219.py
@@ -0,0 +1,31 @@
+# 219. Contains Duplicate II
+# Solved
+# Easy
+# Topics
+# premium lock icon
+# Companies
+# Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+
+# Example 1:
+
+# Input: nums = [1,2,3,1], k = 3
+# Output: true
+# Example 2:
+
+# Input: nums = [1,0,1,1], k = 1
+# Output: true
+# Example 3:
+
+# Input: nums = [1,2,3,1,2,3], k = 2
+# Output: false
+
+class Solution(object):
+    def containsNearbyDuplicate(self, nums, k):
+        seen = set()
+        for i in range(len(nums)):
+            if nums[i] in seen:
+                return True
+            seen.add(nums[i])
+            if len(seen) > k:
+                seen.remove(nums[i - k])
+        return False
